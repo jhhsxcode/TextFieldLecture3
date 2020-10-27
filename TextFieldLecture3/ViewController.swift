85c40fca03da6f9198b09ba108dc54e9a198c296
Bob Brown <bob.brown@d214.org>
2020-10-27 12:10:39 -0500

    Added Delegation and primary action methods

--- TextFieldLecture3/ViewController.swift
+++ TextFieldLecture3/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    
+    @IBAction func whenTextFieldReturnButtonPressed(_ sender: Any) {
+        print("Return Pressed")
+    }
+    
 }
 
